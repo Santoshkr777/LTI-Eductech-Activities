4ad5a019019bfabb4a4420dd3408248aecf42ae6
Santosh kumar <57297518+Santoshkr777@users.noreply.github.com>
2023-03-02 14:05:09 +0530

    Start Pattern

--- L&T Edutech-Activities/Week1 Day3_ Looping Statement - Nested For Loop/5. Star Pattern.java
+++ L&T Edutech-Activities/Week1 Day3_ Looping Statement - Nested For Loop/5. Star Pattern.java
@@ -7,10 +7,7 @@ class Main {
     try {
       int n = scn.nextInt();
       for (int i = 1; i <= n; i++) {
-        for (int j = i; j <= n; j++) {
-          if (j < i) {
-            continue;
-          }
+        for (int j = 1; j <= i; j++) {
           System.out.print("*");
         }
         System.out.println();
